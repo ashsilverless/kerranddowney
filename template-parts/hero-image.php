d75c8ec24ca6e02f2cfa0fd0b4d838563a7629a9
Ash - SIlverless <ash@silverless.co.uk>
2018-11-27 10:53:22 +0000

    Fix sub heading

--- template-parts/hero-image.php
+++ template-parts/hero-image.php
@@ -1,7 +1,7 @@
 <section class="sl-hero <?php the_field('height');?>" style="background-image: url(<?php the_field('image'); ?>);">
 	<div class="content-wrapper layer-1 layer" data-depth='-0.51' data-type='parallax'>
 		<h1><?php the_field('hero_heading');?>
-			<?php if( get_sub_field('sub_heading') ): ?>
+			<?php if( get_field('sub_heading') ): ?>
 				<div><span><?php the_field('sub_heading');?></span></div>
 			<?php endif; ?>
 		</h1>
