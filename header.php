7cdcbc7d43ee4853e4ff950e6f55c192690d97c7
Ash - SIlverless <ash@silverless.co.uk>
2019-02-22 10:52:59 +0000

    Fix - misc minor amends

--- header.php
+++ header.php
@@ -10,11 +10,12 @@
 <head>
 	<meta charset="utf8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="http://gmpg.org/xfn/11">
+	<link rel="stylesheet" href="https://use.typekit.net/cky6efi.css">
   <link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon-32x32.png" sizes="32x32" />
   <link rel="icon" type="image/png" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon-16x16.png" sizes="16x16" />
   <link rel="icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" type="image/x-icon" />
-	<?php wp_head(); ?>
+
+<?php wp_head(); ?>
 	
 </head>
 
@@ -239,16 +240,9 @@
 			</ul>
 	
 		<?php   endif;?>
-
-			 
-			 
 			 
 		 </div>
 
-
-
-
-		    
 			<!--<?php
 			wp_nav_menu( array( 
 			    'theme_location' => 'my-custom-menu', 
